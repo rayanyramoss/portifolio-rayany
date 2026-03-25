52a73984a42f054e2ba5222b3ea5bc0767896682
Rayany Ramos <rayanyramos2106@gmail.com>
2026-03-25 13:20:44 -0300

    primeira versão do portfólio

---
+++ script.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const modal = document.getElementById("modalCertificado");
+    const modalImg = document.getElementById("imgCertificado");
+    const legenda = document.getElementById("legenda-modal");
+    const cartoes = document.querySelectorAll(".cartao");
+    const spanFechar = document.querySelector(".fechar-modal");
+
+    cartoes.forEach(cartao => {
+        cartao.onclick = function() {
+            const caminhoImagem = this.getAttribute("data-cert");
+            const tituloCurso = this.querySelector("h4").innerText;
+           
+            if (caminhoImagem && caminhoImagem !== "") {
+                modal.style.display = "block";
+                modalImg.src = caminhoImagem;
+                legenda.innerHTML = `Certificado: ${tituloCurso}`;
+            } else {
+                alert("O certificado para este curso ainda não foi carregado.");
+            }
+        }
+    });
+
+
+    spanFechar.onclick = () => modal.style.display = "none";
+
+   
+    window.onclick = (event) => {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+});
+
